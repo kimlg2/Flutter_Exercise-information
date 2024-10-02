2371786ccf3cf8e8fcca87f3dc12b53fbabe59d3
gimmin-u <kimlg2015@naver.com>
2024-10-03 05:41:56 +0900

    form과 유효성 검사

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -8,12 +8,59 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('비만도 계산기11')
+        title: Text('비만도 계산')
       ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '키'
+              ),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return '키를 입력해주세요!';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 8),
+            TextFormField(
+            decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: '몸무게'
+           ),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return '몸무게를 입력해주세요!';
+                }
+                return null;
+              },
+            ),
+           const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                if(_formKey.currentState?.validate() ?? false) {
+
+                }
+              },
+              child: Text('결과'),
+            ),
+          ],
+        ),
+      )
+      )
     );
   }
 }
