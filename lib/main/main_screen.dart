5a23e1b079d418f275c8a37dfbf23214e97eaa77
gimmin-u <kimlg2015@naver.com>
2024-10-04 04:30:16 +0900

    화면 배경 background 바꾸기

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -51,65 +51,78 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: Text('BMI 계산'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center( // 전체를 가운데로 정렬
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // 수직 중앙 정렬
-              crossAxisAlignment: CrossAxisAlignment.center, // 수평 중앙 정렬
-              mainAxisSize: MainAxisSize.min, // 콘텐츠가 최소한의 공간만 차지하게 설정
-              children: [
-                TextFormField(
-                  controller: _heightController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: '키',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bmi_background.jpg'), // 배경 이미지 경로
+            fit: BoxFit.cover, // 이미지가 화면 전체를 덮도록 설정
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // 수직 중앙 정렬
+                mainAxisSize: MainAxisSize.min, // 콘텐츠가 최소한의 공간만 차지하게 설정
+                children: [
+                  SizedBox(height: 20),
+                  Image.asset(
+                    'assets/images/bmi.png', // 이미지 경로
+                    height: 100, // 이미지의 높이 설정
                   ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '키를 입력해주세요!';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 8),
-                TextFormField(
-                  controller: _weightController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: '몸무게',
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _heightController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '키',
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '키를 입력해주세요!';
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '몸무게를 입력해주세요!';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState?.validate() == false) {
-                      return;
-                    }
-                    save();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultScreen(
-                          height: double.parse(_heightController.text),
-                          weight: double.parse(_weightController.text),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    controller: _weightController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '몸무게',
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '몸무게를 입력해주세요!';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() == false) {
+                        return;
+                      }
+                      save();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ResultScreen(
+                            height: double.parse(_heightController.text),
+                            weight: double.parse(_weightController.text),
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                  child: Text('결과'),
-                ),
-              ],
+                      );
+                    },
+                    child: Text('결과'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
